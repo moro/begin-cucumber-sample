7cf67b57926e3760f3128437ec7976b61adefda0
moro <moronatural@gmail.com>
2010-08-23 01:02:22 +0900

    GREEN impl. locale switching.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :set_locale
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  #
+  private
+  def set_locale
+    if request.env['HTTP_ACCEPT_LANGUAGE']
+      locale = (request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first || I18n.locale)
+      I18n.locale = locale
+    end
+  end
 end
