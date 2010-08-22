f52e7f549baf18196bfb776649297a8f741621d8
moro <moronatural@gmail.com>
2010-08-23 00:45:12 +0900

    ran migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100822154321) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
