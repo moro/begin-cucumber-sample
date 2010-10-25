14e134978445d858ec07d284816824e2dc98e2d0
moro <moronatural@gmail.com>
2010-10-25 15:30:05 +0900

    日付を相対日時に書き直した

--- features/step_definitions/messages_steps.rb
+++ features/step_definitions/messages_steps.rb
@@ -1,10 +1,14 @@
 
 前提 /^以下のユーザごとのメッセージがある:$/ do |table|
   table.rows.each do |login, message_text, created_at|
-    u = User.find_by_login(login)
-    raise ActiveRecord::RecordNotFound unless u
-    u.messages.create!(:text => message_text, :created_at => created_at)
+    When %Q["#{login}"が"#{created_at}"に"#{message_text}"と投稿している]
   end
+end
+
+前提 /^"([^"]*)"が"([^"]*)"に"([^"]*)"と投稿している$/ do |login, created_at, message_text|
+  u = User.find_by_login(login)
+  raise ActiveRecord::RecordNotFound unless u
+  u.messages.create!(:text => message_text, :created_at => created_at)
 end
 
 ならば /^以下のメッセージが表示されていること:$/ do |expected|
---
+++ features/support/transform.rb
@@ -0,0 +1,5 @@
+Transform /(\d+)時間前/ do |h|
+  Integer(h).hours.ago.iso8601
+end
+
+
