0ff138e027f55253b0a716728106ec49898ab648
moro <moronatural@gmail.com>
2010-11-13 00:58:54 +0900

    update rails 3 syntax for app.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
   belongs_to :user
-  named_scope :descend, {:order => "#{table_name}.created_at DESC"}
+  scope :descend, {:order => "#{table_name}.created_at DESC"}
 
-  named_scope :watched_by, lambda { |watcher|
+  scope :watched_by, lambda { |watcher|
     watchees = watcher.watchings.map(&:watchee_id)
     {:conditions => ["#{table_name}.user_id IN (?)", watchees << watcher.id]}
   }
